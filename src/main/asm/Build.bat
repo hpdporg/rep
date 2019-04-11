50402be4e229d2e3f2ee46e8e895ec288b807947
doenn <lewissall@hotmail.com>
2019-04-11 08:58:54 -0400

    2017

--- src/main/asm/Build.bat
+++ src/main/asm/Build.bat
@@ -1,5 +1,5 @@
 REM CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" AMD64
-link /LARGEADDRESSAWARE /MACHINE:X64 /SUBSYSTEM:WINDOWS  /ENTRY:start dtest.obj "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x64\User32.Lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x64\Kernel32.Lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x64\Gdi32.lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x64\Ws2_32.lib"  /OUT:dtest.exe
-xcopy /Y /E dtest.obj ..\..\..\lib
+link /LARGEADDRESSAWARE /MACHINE:X64 /SUBSYSTEM:WINDOWS  /ENTRY:start rep.obj "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x64\User32.Lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x64\Kernel32.Lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x64\Gdi32.lib" "C:\Program Files (x86)\Windows Kits\10\Lib\10.0.17134.0\um\x64\Ws2_32.lib"  /OUT:rep.exe
+xcopy /Y /E rep.obj ..\..\..\lib
